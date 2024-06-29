31659c729a1b01ef2f5d3469fbca5c3e701079cc
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-06-29 11:53:33 -0300

    add Card PAge

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/pages/card_detail_page.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({super.key});
@@ -15,16 +16,61 @@ class _CardPageState extends State<CardPage> {
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           width: double.infinity,
-          child: const Card(
-            elevation: 8, 
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Text('Meu Card'),
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          const CardDetailPage()));
+            },
+            child: Card(
+              elevation: 8,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 20,
+                          child: Image.network(
+                            'https://c5gwmsmjx1.execute-api.us-east-1.amazonaws.com/prod/dados_processo_seletivo/logo_empresa/119818/LOGO-DIO-COLOR.png_name_20221031-2831-ekn5hh.png',
+                          ),
+                        ),
+                        const Text(
+                          '  Meu Card',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Ler mais',
+                            style:
+                                TextStyle(decoration: TextDecoration.underline),
+                          )),
+                    )
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
