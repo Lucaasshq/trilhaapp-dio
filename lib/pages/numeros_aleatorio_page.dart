475d03d61b54add1eb26d2f2596f168088ad6fb6
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-07-10 09:48:05 -0300

    .

--- lib/pages/numeros_aleatorio_page.dart
+++ lib/pages/numeros_aleatorio_page.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NumeroAleatoriosPage extends StatefulWidget {
   const NumeroAleatoriosPage({super.key});
@@ -11,7 +12,23 @@ class NumeroAleatoriosPage extends StatefulWidget {
 }
 
 class _NumeroAleatoriosPageState extends State<NumeroAleatoriosPage> {
-  int randomNumber = 0;
+  int? randomNumber = 0;
+  // ignore: non_constant_identifier_names
+  final CHAVE_NUMERO_ALEATORIO = 'numero_aleatorio';
+
+  @override
+  void initState() {
+    super.initState();
+    carregarDados();
+  }
+
+  void carregarDados() async {
+    final storage = await SharedPreferences.getInstance();
+    setState(() {
+      randomNumber = storage.getInt(CHAVE_NUMERO_ALEATORIO);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -23,16 +40,25 @@ class _NumeroAleatoriosPageState extends State<NumeroAleatoriosPage> {
           alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text(randomNumber.toString())],
+            children: [
+              Text(
+                randomNumber.toString(),
+                style: const TextStyle(
+                  fontSize: 22,
+                ),
+              )
+            ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
+          onPressed: () async {
+            final storage = await SharedPreferences.getInstance();
             var random = Random();
 
             setState(() {
               randomNumber = random.nextInt(1000);
             });
+            storage.setInt(CHAVE_NUMERO_ALEATORIO, randomNumber!);
           },
           child: const Icon(Icons.add),
         ),
