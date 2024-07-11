655a128bbd5f0173b4c6381257cc98f8087bc156
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-07-11 10:08:52 -0300

    .

--- lib/pages/configuracoes_page.dart
+++ lib/pages/configuracoes_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: non_constant_identifier_names, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ConfiguracoesPage extends StatefulWidget {
