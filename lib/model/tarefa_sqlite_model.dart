142fa72ccfe54e020157cb68975986cd85361130
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-07-20 17:29:08 -0300

    add database

--- lib/model/tarefa_sqlite_model.dart
+++ lib/model/tarefa_sqlite_model.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: unnecessary_getters_setters
+
 class TarefaSQLiteModel {
   int _id = 0;
   String _descricao = '';
   bool _concluido = false;
 
-  TarefaSQLiteModel(this._descricao, this._concluido);
+  TarefaSQLiteModel(this._id ,this._descricao, this._concluido);
 
   int get id => _id;
 
@@ -11,19 +13,17 @@ class TarefaSQLiteModel {
     _id = id;
   }
 
-  // ignore: unnecessary_getters_setters
+  
   String get descricao => _descricao;
 
-  // ignore: avoid_return_types_on_setters
-  void set descricao(String descricao) {
+  
+  set descricao(String descricao) {
     _descricao = descricao;
   }
 
-  // ignore: unnecessary_getters_setters
   bool get concluido => _concluido;
 
-  // ignore: avoid_return_types_on_setters
-  void set concluido(bool concluido) {
+  set concluido(bool concluido) {
     _concluido = concluido;
   }
 }
--- lib/pages/service/repositories/sqlite/tarefa_sqlite_repository.dart
+++ lib/pages/service/repositories/sqlite/tarefa_sqlite_repository.dart
@@ -1,3 +1,28 @@
+import 'package:trilhaapp/model/tarefa_sqlite_model.dart';
+import 'package:trilhaapp/pages/service/repositories/sqlite/sqlite_database.dart';
+
 class TarefaSQLiteRepository {
-  
+  Future<List<TarefaSQLiteModel>> obterDados() async {
+    List<TarefaSQLiteModel> tarefas = [];
+    var db = await SQLiteDataBase().obterDataBase();
+    var result = await db.rawQuery('SELECT id, descricao, concluido FROM tarefas');
+    for (var element in result) {
+      tarefas.add(
+        TarefaSQLiteModel(
+          int.parse(element['id'].toString()),
+          element['descricao'].toString(),
+          element['concluido'] == '1',
+        ),
+      );
+    }
+    return tarefas;
+  }
+
+  Future<void> salvar(TarefaSQLiteModel tarefaSQLIteModel) async {
+    var db = await SQLiteDataBase().obterDataBase();
+    db.rawInsert(
+      'INSERT INTO tarefas (descricao, concluido) values(?,?)',
+      [tarefaSQLIteModel.descricao, tarefaSQLIteModel.concluido],
+    );
+  }
 }
