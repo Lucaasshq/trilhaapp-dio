3024a1f9caf496aa2d04466f4b6fa42cb628564c
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-07-24 22:14:56 -0300

    Create class PostModel and PostModelRepository

---
+++ lib/pages/repositories/post_model_repository.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:trilhaapp/model/post_model.dart';
+import 'package:http/http.dart' as http;
+
+class PostModelRepository {
+  Future<List<PostModel>> getPost() async {
+    var response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    if (response.statusCode == 200) {
+      var jsonPost = jsonDecode(response.body);
+      return (jsonPost as List).map((post) => PostModel.fromJson(post)).toList();
+    } else {
+      return [];
+    }
+  }
+}
